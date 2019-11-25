5d358223f657ae3182c023d5e4530bf708f79514
jakejcarroll <57533010+jakejcarroll@users.noreply.github.com>
2019-11-25 10:52:29 +0000

    Add files via upload

--- sensor.py
+++ sensor.py
@@ -28,17 +28,18 @@ while True:
 	readings_gas = gas.read_all()
 	
 	# Get the temperature of the CPU for compensation
-	def get_cpu_temperature():
-    	with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
-        	temp = f.read()
-       	 	temp = int(temp) / 1000.0
-  	  		return temp
+	#def get_cpu_temperature():
+    	#with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        	#temp = f.read()
+       	 	#temp = int(temp) / 1000.0
+  	  		#return temp
     
     # Tuning factor for compensation. Decrease this number to adjust the
 	# temperature down, and increase to adjust up
-	factor = 0.8
+	
+	#factor = 0.8
 
-	cpu_temps = [get_cpu_temperature()] * 5
+	#cpu_temps = [get_cpu_temperature()] * 5
 	
 	#Format data
 	pressure_in = 0.03937008*(pressure_mb)
@@ -52,16 +53,16 @@ while True:
 	gas_oxidising = (readings_gas.oxidising)
 	gas_nh3 = (readings_gas.nh3)
 	
-	cpu_temp = get_cpu_temperature()
+	#cpu_temp = get_cpu_temperature()
     # Smooth out with some averaging to decrease jitter
-    cpu_temps = cpu_temps[1:] + [cpu_temp]
-    avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
-    raw_temp = bme280.get_temperature()
-    comp_temp = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
+    #cpu_temps = cpu_temps[1:] + [cpu_temp]
+    #avg_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+    #raw_temp = bme280.get_temperature()
+    #comp_temp = raw_temp - ((avg_cpu_temp - raw_temp) / factor)
 	
 	
 	#Stream data
-	streamer.log("Temperature " +  "(C): ", comp_temp)
+	streamer.log("Temperature " +  "(C): ", temp_c)
 	streamer.log("Humidity " + "%: ", humidity)
 	streamer.log("Pressure "+"(IN) ", pressure_in)
 	streamer.log("PM1.0 "+ "(ug/m3) ", pm1)
